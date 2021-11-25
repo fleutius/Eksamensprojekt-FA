d5824c483fcc82319dc7452d014c9e43149fe764
fleutius <flemming88@gmail.com>
2021-11-25 23:03:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import hashlib
+
 ##Design Fase 
 
 #Requerements list:
@@ -24,13 +27,57 @@ python hashing
 
 """
 
-class Bruger:
-    
-    def __init__(self, first_name, last_name, email, password, profil):
-        self.name = self.first_name + ' ' + self.last_name
+class User:
+    users = []
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.name = (first_name + ' ' + last_name)
         self.email = email
-        self.password = password
-        self.profil = profil
+        self.salt = os.urandom(32)
+        self.password = input("Please enter your password: ")
+        self.key = hashlib.pbkdf2_hmac(
+            'sha256',
+            self.password.encode('utf8'),
+            self.salt,
+            100000,
+            dklen=128
+        )
+        self.storedpass = self.salt + self.key
+        self.password = ''
+        self.users.append(self.email)
 
-    
 
+    def create_user():
+        user=User(
+            first_name = input("Please enter Users first name: "),
+            last_name = input("Please enter Users last name: "),
+            email = input("Please enter Users email: ")
+        )
+
+
+def main():
+    go_on = True
+    while go_on == True:
+        try:
+            print("Welcome to the User creation tool")
+            print("please be aware, that user passwords are not stored, and it is not possible for admins to return a users password, if it is lost or forgotten ")
+            print()
+            User.create_user()
+            print()
+            print("List of current users: ")
+            print(User.users)
+            state = input("Do you wish to add another user? (y/n): ").lower
+            if state == "y":
+                main()
+            elif state == "n":
+                print("Thank you for using the User creation tool. ")
+                go_on == False
+            else: raise ValueError
+        except ValueError:
+            print("Please enter correct value")
+    else:
+        print("Goodbye")
+
+
+main()
